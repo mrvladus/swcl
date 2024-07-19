1664e6b41aa82508db6be4538d6bc2949617ac0e
mrvladus <mrvladus@yandex.ru>
2024-07-19 16:43:46 +0300

    improve tests

--- tests/test-application.c
+++ tests/test-application.c
@@ -3,11 +3,20 @@
 
 SWCLTestResult test_swcl_application_new() {
   SWCLTestResult res = {0, 0};
-  SWCL_LOG("Test: Create Application");
   if (!swcl_application_new("io.github.mrvladus.Test"))
     res.failed = 1;
   else
     res.passed = 1;
+  return res;
+}
 
+SWCLTestResult test_swcl_application_get_cursor_position() {
+  SWCLTestResult res = {0, 0};
+  SWCLApplication *app = swcl_application_new("io.github.mrvladus.Test");
+  SWCLPoint point = swcl_application_get_cursor_position(app);
+  if (point.x != 0 || point.y != 0)
+    res.failed = 1;
+  else
+    res.passed = 1;
   return res;
 }
--- tests/tests.c
+++ tests/tests.c
@@ -1,15 +1,23 @@
 #include "tests.h"
 #include "test-application.c"
+#include <stdint.h>
 #include <stdio.h>
 
 SWCLTestsResults test_results = {0, 0, 0};
 
-void run_test(SWCLTestResult (*fn)()) {
+void run_test(SWCLTestResult (*fn)(), const char *name) {
   printf("====================================\n");
-  printf("Run test: %d\n", ++test_results.total_run);
+  printf("TEST: %s\n", name);
   SWCLTestResult res = fn();
   test_results.total_passed += res.passed;
   test_results.total_failed += res.failed;
+  test_results.total_run++;
+  const char *msg;
+  if (res.passed == 1)
+    msg = "PASSED";
+  else
+    msg = "FAILED";
+  printf("RESULT: %s\n", msg);
   printf("====================================\n");
 }
 
@@ -24,7 +32,9 @@ uint8_t finish_testing() {
 }
 
 int main(int argc, char const *argv[]) {
-  run_test(test_swcl_application_new);
+  run_test(test_swcl_application_new, "swcl_application_new");
+  run_test(test_swcl_application_get_cursor_position,
+           "test_swcl_application_get_cursor_position");
 
   return finish_testing();
 }
